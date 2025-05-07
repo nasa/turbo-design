1f3aa91b50e4a9a6ec61e3446608378387849028
Paht J <paht.juangphanich@gmail.com>
2025-05-07 16:47:19 -0400

    checking sign convention

--- turbodesign/radeq.py
+++ turbodesign/radeq.py
@@ -99,16 +99,16 @@ def radeq(row:BladeRow,upstream:BladeRow,downstream:BladeRow=None) -> BladeRow:
         if (C>1) & ((gamma/(gamma-1))<2):
             raise Exception("Invalid value of C {C}, change reduce alpha or Vm")
         B = (1-C)**(gamma/(gamma-1))
-        A = P0 * gamma/(gamma-1) * (1-C)**(1/(gamma-1)) * (1 + np.tan(alpha)**2)/(2*Cp)
+        A = -P0 * gamma/(gamma-1) * (1-C)**(1/(gamma-1)) * (1 + np.tan(alpha)**2)/(2*Cp)
         
-        eqn15_rhs = (Vt**2/r + Vm**2/rm*np.cos(phi) - Vr*dVm_dm) # right hand side of equation 15
-        eqn15_rhs_simple = Vt**2/r # right hand side of equation 15 simplified 
+        eqn15_rhs = (-Vt**2/r - Vm**2/rm*np.sin(phi) + Vr*dVm_dm) # right hand side of equation 15
+        eqn15_rhs_simple = -Vt**2/r # right hand side of equation 15 simplified 
         
         epsilon = 1e-10  # or another small threshold
         if abs(rm) > epsilon:
-            dVm_dr = -T0/(2*Vm*A) * (rho*eqn15_rhs - B*dP0_dr) + Vm/(2*T0) * dT0_dr # Eqn 21
+            dVm_dr = T0/(2*Vm*A) * (rho*eqn15_rhs - B*dP0_dr) + Vm/(2*T0) * dT0_dr # Eqn 21
         else:
-            dVm_dr = -T0/(2*Vm*A) * (rho*eqn15_rhs_simple - B*dP0_dr) + Vm/(2*T0) * dT0_dr  # Eqn 21, simple 
+            dVm_dr = T0/(2*Vm*A) * (rho*eqn15_rhs_simple - B*dP0_dr) + Vm/(2*T0) * dT0_dr  # Eqn 21, simple 
         
         ydot = np.array([dP0_dr,dT0_dr,dVm_dr])
 
@@ -129,25 +129,26 @@ def radeq(row:BladeRow,upstream:BladeRow,downstream:BladeRow=None) -> BladeRow:
     # We are solving for the values of these quantities at row exit
     ics = np.array([P0m,T0m,Vmm])
 
-    hub_to_tip = np.linspace(hub_radius,tip_radius)
-    res = odeint(ode_radeq_streamtube, ics, hub_to_tip, tfirst=True)
+    # hub_to_tip = np.linspace(hub_radius,tip_radius)
+    # res = odeint(ode_radeq_streamtube, ics, hub_to_tip, tfirst=True)
     
-    P0_new = interp1d(hub_to_tip,res[:,0])(row_radius)
-    T0_new = interp1d(hub_to_tip,res[:,1])(row_radius)
-    Vm_new = interp1d(hub_to_tip,res[:,2])(row_radius)
-    # mean_radius_to_tip = np.linspace(mean_radius,tip_radius)
-    # res1 = odeint(ode_radeq_streamtube, ics, mean_radius_to_tip, tfirst=True)
+    # P0_new = interp1d(hub_to_tip,res[:,0])(row_radius)
+    # T0_new = interp1d(hub_to_tip,res[:,1])(row_radius)
+    # Vm_new = interp1d(hub_to_tip,res[:,2])(row_radius)
     
-    # mean_radius_to_hub = np.flip(np.linspace(hub_radius,mean_radius))
-    # res2 = odeint(ode_radeq_streamtube, ics, mean_radius_to_hub, tfirst=True)
+    mean_radius_to_tip = np.linspace(mean_radius,tip_radius)
+    res1 = odeint(ode_radeq_streamtube, ics, mean_radius_to_tip, tfirst=True)
     
-    # res2 = np.flipud(res2)
-    # res = np.concatenate([res2[:-1,:],res1])
-    # r = np.concatenate([np.flip(mean_radius_to_hub)[:-1], mean_radius_to_tip])
+    mean_radius_to_hub = np.flip(np.linspace(hub_radius,mean_radius))
+    res2 = odeint(ode_radeq_streamtube, ics, mean_radius_to_hub, tfirst=True)
     
-    # P0_new = interp1d(r,res[:,0])(row_radius)
-    # T0_new = interp1d(r,res[:,1])(row_radius)
-    # Vm_new = interp1d(r,res[:,2])(row_radius)
+    res2 = np.flipud(res2)
+    res = np.concatenate([res2[:-1,:],res1])
+    r = np.concatenate([np.flip(mean_radius_to_hub)[:-1], mean_radius_to_tip])
+    
+    P0_new = interp1d(r,res[:,0])(row_radius)
+    T0_new = interp1d(r,res[:,1])(row_radius)
+    Vm_new = interp1d(r,res[:,2])(row_radius)
     
     row.P0 = P0_new
     row.T0 = T0_new
