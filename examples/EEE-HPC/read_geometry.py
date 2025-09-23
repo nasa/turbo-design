ae91f3b764fc9bca058fc5b70e55427ad98f7638
Paht <PahtNASA@nasa.gov>
2025-09-23 14:52:14 -0400

    eee_hpc geometry read

--- examples/EEE-HPC/read_geometry.py
+++ examples/EEE-HPC/read_geometry.py
@@ -1,6 +1,10 @@
+from typing import List
 import numpy as np
+import numpy.typing as npt
 import os, pyiges, pickle
 import matplotlib.pyplot as plt
+from pyiges.geometry import RationalBSplineCurve  # <-- import the class
+
 
 def Process_HubShroud_IGES():
     """Exports hub and shroud curves in inches
@@ -31,83 +35,58 @@ def Process_HubShroud_IGES():
 def ProcessBlades_IGES():
     """Exports stator and rotor curves in inches
     """
- 
-    # load an example impeller
-    iges_rotor1 = pyiges.read('geometry/compr_rotor1.igs')
-    iges_stator1 = pyiges.read('geometry/compr_stator1.igs')
-
-    iges_rotor2 = pyiges.read('geometry/compr_rotor2.igs')
-    iges_stator2 = pyiges.read('geometry/compr_stator1.igs')
     
-    iges_rotor3 = pyiges.read('geometry/compr_rotor3.igs')
-    iges_stator3 = pyiges.read('geometry/compr_stator3.igs')
-    
-    iges_rotor4 = pyiges.read('geometry/compr_rotor4.igs')
-    iges_stator4 = pyiges.read('geometry/compr_stator4.igs')
-    
-    iges_rotor5 = pyiges.read('geometry/compr_rotor5.igs')
-    iges_stator5 = pyiges.read('geometry/compr_stator5.igs')
-    
-    iges_rotor6 = pyiges.read('geometry/compr_rotor6.igs')
-    iges_stator6 = pyiges.read('geometry/compr_stator6.igs')
-    
-    iges_rotor7 = pyiges.read('geometry/compr_rotor7.igs')
-    iges_stator7 = pyiges.read('geometry/compr_stator7.igs')
-    
-    iges_rotor8 = pyiges.read('geometry/compr_rotor8.igs')
-    iges_stator8 = pyiges.read('geometry/compr_stator8.igs')
-    
-    iges_rotor9 = pyiges.read('geometry/compr_rotor9.igs')
-    iges_stator9 = pyiges.read('geometry/compr_stator9.igs')
-    
-    iges_rotor10 = pyiges.read('geometry/compr_rotor10.igs')
-    iges_stator10 = pyiges.read('geometry/compr_stator10.igs')
-    
-    curve_delta=0.001
-    # Stage 1
-    os.makedirs('csv',exist_ok=True)
-
-    stator_pts1 = list(); indx = 1
-    for i in range(2,7):
-        curve = iges_stator1.items[i].to_geomdl()
-        curve.delta=curve_delta
-        points = np.array(curve.evalpts); n = points.shape[0]
-        stator_pts1.append(points)
+    def extract_curves(name:str, iges:pyiges.Iges) -> List[npt.NDArray]:
+        curve_delta=0.001
         os.makedirs('csv', exist_ok=True)
-        np.savetxt(f'csv/stator1_{indx}.csv',points,fmt="%f",delimiter=',',header='x,rtheta,r')
-        indx+=1
+        sections = list() # These are the cuts at different radius
+        for i in range(len(iges.items)): # these are the cut section
+            if isinstance(iges.items[i], RationalBSplineCurve):
+                curve = iges.items[i].to_geomdl()
+                curve.delta=curve_delta
+                points = np.array(curve.evalpts); n = points.shape[0]
+                sections.append(points)
+                np.savetxt(f'csv/{name}.csv',points,fmt="%f",delimiter=',',header='x,rtheta,r')
+        return sections
+            
+        
+    # load an example impeller
+    igv = extract_curves("igv", pyiges.read('geometry/compr_igv.igs'))
+    rotor1 = extract_curves("rotor1", pyiges.read('geometry/compr_rotor1.igs'))
+    stator1 = extract_curves("stator1", pyiges.read('geometry/compr_stator1.igs'))
 
-    # print an invidiual entity (boring)
-    rotor_pts1 = list(); indx = 1
-    for i in range(2,7):
-        curve = iges_rotor1.items[i].to_geomdl()
-        curve.delta=curve_delta
-        points = np.array(curve.evalpts);
-        rotor_pts1.append(points)
-        np.savetxt(f'csv/rotor1_{indx}.csv',points,fmt="%f",delimiter=',',header='x,rtheta,r')
-        indx+=1
-
-    # Stage 2
-    stator_pts2 = list(); indx = 1
-    for i in range(2,6):
-        curve = iges_stator2.items[i].to_geomdl()
-        curve.delta=curve_delta
-        points = np.array(curve.evalpts)
-        stator_pts2.append(points)
-        np.savetxt(f'csv/stator2_{indx}.csv',points,fmt="%f",delimiter=',',header='x,rtheta,r')
-        indx+=1
-
-    # print an invidiual entity (boring)
-    rotor_pts2 = list(); indx = 1
-    for i in range(2,7):
-        curve = iges_rotor2.items[i].to_geomdl()
-        curve.delta=curve_delta
-        points = np.array(curve.evalpts)
-        rotor_pts2.append(points)
-        np.savetxt(f'csv/rotor2_{indx}.csv',points,fmt="%f",delimiter=',',header='x,rtheta,r')
-        indx+=1
-
-    pickle.dump([stator_pts1,rotor_pts1,stator_pts2,rotor_pts2],open('stator_rotor.pkl','wb'))
+    rotor2 = extract_curves("rotor2", pyiges.read('geometry/compr_rotor2.igs'))
+    stator2 = extract_curves("stator2", pyiges.read('geometry/compr_stator2.igs'))
+    
+    rotor3 = extract_curves("rotor3", pyiges.read('geometry/compr_rotor3.igs'))
+    stator3 = extract_curves("stator3", pyiges.read('geometry/compr_stator3.igs'))
+    
+    rotor4 = extract_curves("rotor4", pyiges.read('geometry/compr_rotor4.igs'))
+    stator4 = extract_curves("stator4", pyiges.read('geometry/compr_stator4.igs'))
+    
+    rotor5 = extract_curves("rotor5", pyiges.read('geometry/compr_rotor5.igs'))
+    stator5 = extract_curves("stator5", pyiges.read('geometry/compr_stator5.igs'))
+    
+    rotor6 = extract_curves("rotor6", pyiges.read('geometry/compr_rotor6.igs'))
+    stator6 = extract_curves("stator6", pyiges.read('geometry/compr_stator6.igs'))
+    
+    rotor7 = extract_curves("rotor7", pyiges.read('geometry/compr_rotor7.igs'))
+    stator7 = extract_curves("stator7", pyiges.read('geometry/compr_stator7.igs'))
+    
+    rotor8 = extract_curves("rotor8", pyiges.read('geometry/compr_rotor8.igs'))
+    stator8 = extract_curves("stator8", pyiges.read('geometry/compr_stator8.igs'))
+    
+    rotor9 = extract_curves("rotor9", pyiges.read('geometry/compr_rotor9.igs'))
+    stator9 = extract_curves("stator9", pyiges.read('geometry/compr_stator9.igs'))
+    
+    rotor10 = extract_curves("rotor10", pyiges.read('geometry/compr_rotor10.igs'))
+    stator10 = extract_curves("stator10", pyiges.read('geometry/compr_stator10.igs'))
+    
+    pickle.dump([igv,rotor1,stator1,rotor2,stator2,
+                 rotor3,stator3,rotor4,stator4,
+                 rotor5,stator5,rotor6,stator6,
+                 rotor7,stator7,rotor8,stator8,
+                 rotor9,stator9,rotor10,stator10],open('rotor_stator.pkl','wb'))
 
 if __name__ == "__main__":
     Process_HubShroud_IGES()
